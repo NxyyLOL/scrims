4d3c7b3c08031c1414b1131dd54346f2f31155fa
nxyy <175990828+nxyystore@users.noreply.github.com>
2026-03-13 21:14:13 -0400

    feat: add hub.lua

---
+++ hub.lua
@@ -0,0 +1,11 @@
+
+local Scripts = {
+    [14044547200] = "https://raw.githubusercontent.com/NxyyLOL/scrims/refs/heads/main/script/utg.lua", -- utg
+}
+
+local Script = Scripts[game.GameId]
+if Script then
+    loadstring(game:HttpGet(Script))()
+else
+    game:GetService("Players").LocalPlayer:Kick("Script is currently unavaliable for this game!")
+end
